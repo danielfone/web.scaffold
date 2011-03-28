6fe6fadbf166abb03120ab98dcd25443c1f0c575
Daniel Fone <daniel.fone@gmail.com>
2011-03-28 20:07:08 +1300

    Skeletal rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebScaffold::Application.config.secret_token = '4dbe8797449ef74842584c393e2e83c1a51afa6c26cc207f1d7e4cfe4ff520907ee693fd962ad229258b2cca0bbcf408510cd1ceee3306885e046788af2374f8'
